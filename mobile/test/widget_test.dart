553e1c0998f4e5e51d7c0a9d2ca39215e63b270a
Daniel Arrigo <arrigo1888@gmail.com>
2026-07-11 09:46:03 +0700

    Fix widget test looking for deleted SIDEPAK text

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -9,7 +9,6 @@ void main() {
       (tester) async {
     await tester.pumpWidget(const MyApp());
     await tester.pump();
-    expect(find.text('SIDEPAK'), findsOneWidget);
     expect(find.text('Masuk'), findsOneWidget);
     // 'Daftar Sekarang' is part of the longer string 'Belum memiliki akun? Daftar Sekarang'
     expect(find.textContaining('Daftar Sekarang'), findsOneWidget);
